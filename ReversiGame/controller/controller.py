f30cffa36058981c387434ce73691fa6516fa96f
Roberto <chadwick.r@northeastern.edu>
2023-03-13 12:01:56 -0400

    updated controller to add flask

--- ReversiGame/controller/controller.py
+++ ReversiGame/controller/controller.py
@@ -1,6 +1,8 @@
+from flask import Flask
 from model.game import Reversi
 from view.view import ConsoleGameView
 
+app = Flask(__name__)
 class GameController():
     
     def __init__(self):
@@ -46,5 +48,48 @@ class GameController():
         current_board = self.game.board.get_grid()
         current_player = self.game.cur_player.value
         self.view.print_board(current_board, current_player)
-        self.view.print_winner(self.game.player1_score, self.game.player2_score)  
+        self.view.print_winner(self.game.player1_score, self.game.player2_score)
+                #make a game obj that contains board
+
+        #add game obj to array games
+
+        @app.route("/")
+        def root_Web()
+        # here returns the home page of the front end with possible options to login
+            return "<p>Test</p>"
+
+        @app.route("/login")
+        def login(username, password)
+        # if guest frontend please return null username and password. 
+        # if username password empty = guest;
+        # need to make another file to store userinfomation so I can referance
+            return "<p>Test</p>"
+
+        @app.route("/make_move")
+        def make_move(game_ID, x_pos, y_pos, current_player)
+        # check if valid move first if not valid return invalid move (is_move_possible)
+        # make_move(x, y)
+            return "<p>New Board as a JSON</p>"
+        
+        @app.route("/possible_moves")
+        def possible_moves(game_ID)
+        # check array games for game_ID
+        # check with model possible moves possible_moves(self)
+        # make JSON for front end
+        # check for possible moves and return JSON of possible moves so the front end can display all possible moves by player highlighted
+            return "<p>Possible Moves as a JSON</p>"
+
+        @app.route("/game_state")# this will serve as a function to look for games
+        def game_state(game_ID)
+            return "<p>Game is Awaiting players, or Game Started (With players turn)</p>"
+
+        @app.route("/create_game")# do we create games with one player and put them on a lobby for others to join, or do we create the games with the two usernames
+        def create_game(player1, player2)
+            return "<p>Game ID</p>"
+
+        @app.route("/join_game")
+        def join_game(game_ID, player_joining)
+        # join player to game_ID and send empty board to start, also return starting player
+            return "<p>Empty Board as JSON</p>"
+
 
