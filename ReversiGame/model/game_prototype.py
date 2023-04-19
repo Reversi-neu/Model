146e1a52376ffd5ede5807c385d4c84a771f7fb6
Lucas <77511972+lucaslaya@users.noreply.github.com>
2023-04-19 13:44:53 -0400

    Update game_prototype.py

--- ReversiGame/model/game_prototype.py
+++ ReversiGame/model/game_prototype.py
@@ -2,6 +2,19 @@ from abc import ABC, abstractmethod
 
 # Interface for game prototype
 class GamePrototype(ABC):
+    """
+    Prototype interface for the game object
+    
+    Methods
+    -------
+    copy()
+        creates a copy of the game object
+    """
+    
     @abstractmethod
     def copy(self):
+        """
+        Creates a copy of the object
+        """
+        
         pass
