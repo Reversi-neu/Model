32c4004c6f2cc5fea3da8a65e2e9d50a0f62c5d7
Lucas <77511972+lucaslaya@users.noreply.github.com>
2023-04-19 15:47:26 -0400

    Update reversi_interface.py

--- ReversiGame/model/reversi_interface.py
+++ ReversiGame/model/reversi_interface.py
@@ -2,6 +2,24 @@ from abc import ABC, abstractmethod
 
 # Interface for Reversi game
 class ReversiInterface(ABC):
+    """
+    An interface class to use for the game proxy
+        
+    Methods
+    -------
+    isValidMove(board, move):
+        Checks if move made is legal
+    makeMove(move):
+        Makes a move on the board and updates the tiles
+    possibleMoves():
+        Returns array of possible moves for the player
+    checkWin():
+        Checks the state of the board
+    changeCurPlayer():
+        Changes the current player
+    copy():
+        Copies the game
+    """
 
     @abstractmethod
     def isValidMove(self, move):
