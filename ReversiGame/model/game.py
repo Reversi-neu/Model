8c06c65a00c6636b28f78396de664531294e2f29
Lucas <77511972+lucaslaya@users.noreply.github.com>
2023-04-19 15:54:05 -0400

    Update game.py

--- ReversiGame/model/game.py
+++ ReversiGame/model/game.py
@@ -6,7 +6,41 @@ from model.game_prototype import GamePrototype
 
 # Reversi class - the main class of the game
 class Reversi(ReversiInterface, GamePrototype):
-    # ai_depth 0 means no AI in game
+    """
+    The main reversi game class
+    
+    Attributes
+    ----------
+    board : Board
+        Stores the board object
+    boardSize : int
+        Stores the board size
+    curPlayer : Player()
+        Keeps track of the current player
+    gameLogic : GameLogic()
+        The rules to use when making moves and checking
+        for the win
+    player1score : int
+        Player 1 score
+    player2Score : int
+        Player 2 score
+        
+    Methods
+    -------
+    isValidMove(board, move):
+        Checks if move made is legal
+    makeMove(move):
+        Makes a move on the board and updates the tiles
+    possibleMoves():
+        Returns array of possible moves for the player
+    checkWin():
+        Checks the state of the board
+    changeCurPlayer():
+        Changes the current player
+    copy():
+        Copies the game
+    """
+    
     def __init__(self, size=8, gameLogic=None): 
         if gameLogic is None:
                 gameLogic = ClassicMode()
@@ -27,11 +61,33 @@ class Reversi(ReversiInterface, GamePrototype):
 
     # Check if the move is valid, return False if not valid
     def isValidMove(self, move):
+        """
+        Returns if a move is valid or not
+        
+        Parameters
+        ----------
+        move : int[]
+            Coordinate of the move
+            
+        Returns
+        -------
+        boolean : is the turn valid
+        """
+        
         # returns False if not valid
         return self.gameLogic.isMovePossible(self.board.getGrid(), move, self.curPlayer.value)
     
     # Make a move on the board
     def makeMove(self, move):
+        """
+        Makes a move on the board according the the gamelogic and the given move
+        
+        Parameters
+        ----------
+        move : int[]
+            Coordinate of the move
+        """
+        
         gainedTiles = self.gameLogic.makeMove(self.board.getGrid(), move, self.curPlayer.value)
 
         for tile in gainedTiles:
@@ -39,6 +95,10 @@ class Reversi(ReversiInterface, GamePrototype):
     
     # Change the current player
     def changeCurPlayer(self):
+        """
+        Changes the current player to the next one
+        """
+        
         if self.curPlayer == Player.black:
             self.curPlayer = Player.white
         else:
@@ -46,6 +106,14 @@ class Reversi(ReversiInterface, GamePrototype):
     
     # Checks for a winner, returns 0 if no winner, 1 if player 1 won, 2 if player 2 won
     def checkWin(self):
+        """
+        Checks if the game is over
+            
+        Returns
+        -------
+        str : state of the game
+        """
+        
         self.player1Score = 0
         self.player2Score = 0
         for i in range(self.boardSize):
@@ -60,11 +128,27 @@ class Reversi(ReversiInterface, GamePrototype):
 
     # Gets the array of possible moves
     def possibleMoves(self):
+        """
+        Checks all of the possible moves for the current player
+            
+        Returns
+        -------
+        int[] : array of legal move coordinate
+        """
+        
         # array of moves [0, 0]
         return self.gameLogic.possibleMoves(self.board.getGrid(), self.curPlayer.value)
 
     # DESIGN PATTERN: Prototype
     def copy(self):
+        """
+        Create a copy of the current game object.
+            
+        Returns
+        -------
+        Reversi() : Returns a copy of this game object
+        """
+        
         copiedGame = Reversi(self.boardSize, self.gameLogic)
         copiedGame.board = Board(self.boardSize, board=[row.copy() for row in self.board.getGrid()])
         copiedGame.curPlayer = self.curPlayer
