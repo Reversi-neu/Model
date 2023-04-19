971b7d8bc0ec631efc0c68abc007878a05b12fd2
Lucas <77511972+lucaslaya@users.noreply.github.com>
2023-04-19 13:53:35 -0400

    Update reversi_ai.py

--- ReversiGame/model/reversi_ai.py
+++ ReversiGame/model/reversi_ai.py
@@ -1,10 +1,41 @@
 # Reversi AI class - AI implementation for Reversi game
 class ReversiAI:
+    """
+    An AI agent for the reversi game using minimax
+    
+    Attributes
+    ----------
+    depth : int
+        the depth of search for the ai algo
+        
+    Methods
+    -------
+    get_best_move(game)
+        Gets the best next move for the current game
+    minimax(game, depth, maximizing)
+        In charge of the minimax algorithm and scoring moves
+    """
+    
     def __init__(self, depth):
         self.depth = depth
 
     # Returns the best move for the AI
     def get_best_move(self, game):
+        """
+        Gets the best move using minimax
+        
+        Parameters
+        ----------
+            game : Reversi
+                The game object for the AI to run on
+        
+        Returns
+        -------
+            bestMove : int[2]
+                A coordinate of the best next move according
+                to the AI
+        """
+        
         possibleMoves = game.possibleMoves()
         if not possibleMoves:
             return None
@@ -28,15 +59,28 @@ class ReversiAI:
 
         return bestMove
 
-        #print(possibleMoves)
-        #print(game.board.get_grid())
-        #print(game.player1_score)
-        #print(game.player2_score)
-
-        #return possibleMoves[0]
-
     # Minimax algorithm for AI - returns the best score for the AI
     def minimax(self, game, depth, maximizing):
+        """
+        Runs the minimax algorithm
+        
+        Parameters
+        ----------
+            game : Reversi
+                The game object for the AI to run on
+            depth : int
+                How deep the algorithm will search 
+            maximizing : boolean
+                What type of scoring is happening, mini or max
+        
+        Returns
+        -------
+            minEval : int
+                Lowest score turn found if minimizing
+            maxEval : int
+                Highest score turn found if maximizing
+        """
+        
         if depth == 0 or game.checkWin():
             if game.curPlayer == 1:
                 return game.player1Score - game.player2Score
